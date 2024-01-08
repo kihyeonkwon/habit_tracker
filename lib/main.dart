55b20cd614849b41208d84f10b683204877efe97
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2024-01-08 21:04:25 +0900

    init project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/constants/app_assets.dart';
+import 'package:habit_tracker_flutter/constants/app_colors.dart';
+import 'package:habit_tracker_flutter/ui/home/home_page.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppAssets.preloadSVGs();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Helvetica Neue',
+      ),
+      home: AppTheme(
+        data: AppThemeData.defaultWithSwatch(AppColors.red),
+        child: const HomePage(),
+      ),
+    );
+  }
+}
